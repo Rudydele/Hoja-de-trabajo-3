a50034334f21f10d45a6428ff7fcc137378ef6b9
Rudy <rudydleon@ufm.edu>
2022-05-09 19:35:14 -0600

    Ej

---
+++ algoritmos/test.py
@@ -0,0 +1,21 @@
+'''
+from brute_force.divisorsn import divisors
+
+print("***Divisors de n***")
+n = 381
+result = divisors(381)
+print("n: " + str(n) + "divisors: " + str(result))
+'''
+
+'''
+from brute_force.pin_unlock import unlock
+print("***pin unlock***")
+print(unlock("3829"))
+'''
+
+'''
+from brute_force.sum import suma
+print("***sum***")
+n = int(input("ingrese n: "))
+'''
+from lists.largest_number import maximo
